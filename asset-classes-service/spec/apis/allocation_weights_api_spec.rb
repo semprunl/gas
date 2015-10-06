8a99a956f110e19f31a6810e355435abc4b84aa5
Luis Semprun <semprunl@gmail.com>
2015-10-06 11:34:31 +0200

    Implement allocation weight API spec

--- asset-classes-service/spec/apis/allocation_weights_api_spec.rb
+++ asset-classes-service/spec/apis/allocation_weights_api_spec.rb
@@ -7,10 +7,24 @@ end
 describe AllocationWeightsApi do
   include Rack::Test::Methods
 
-  describe 'e.g. GET, POST, PUT, etc.' do
-    it 'needs tests to be written!' do
-      pending('write tests for AllocationWeightsApi!')
-    end
+  it 'gets a list' do
+    FactoryGirl.create_list(:allocation_weight, 2)
+
+    get '/allocation_weights'
+    json = JSON.parse(last_response.body)
+
+    expect(last_response.status).to eq(200)
+    expect(json['data'].length).to eq(2)
   end
 
+  it 'gets a list by tolerance level' do
+    aw = FactoryGirl.create_list(:allocation_weight, 2)
+    aw_tl = aw.first.tolerance_level
+    
+    get "/allocation_weights?tolerance_level=#{aw_tl}"
+
+    json = JSON.parse(last_response.body)
+    expect(last_response.status).to eq(200)
+    expect(json['data'].length).to eq(1)
+  end
 end
