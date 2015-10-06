8170c68051ee9851affd2e32e1070153be2e1e6a
Luis Semprun <semprunl@gmail.com>
2015-10-06 11:35:20 +0200

    Implement readme spec

--- asset-classes-service/spec/docs/readme_spec.rb
+++ asset-classes-service/spec/docs/readme_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
 
+def app
+  ApplicationApi
+end
+
 describe 'README' do
-  it 'has been filled out' do
-    pending 'Please fill out the sections that have a :bow:'
+  include Rack::Test::Methods
+
+  context 'swagger documentation root' do
+    before do
+      get '/swagger_doc'
+      expect(last_response.status).to eq(200)
+      @json = JSON.parse(last_response.body)
+    end
+
+    it 'exposes api version' do
+      expect(@json['apiVersion']).to eq('0.1')
+      expect(@json['apis'].size).to be > 2
+    end
+  end
+
+  context 'swagger documentation api' do
+    before do
+      get '/swagger_doc'
+      expect(last_response.status).to eq(200)
+      @apis = JSON.parse(last_response.body)['apis']
+    end
   end
 end
