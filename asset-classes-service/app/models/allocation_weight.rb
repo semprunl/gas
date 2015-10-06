5d7cf0cc4985aad5d465e6f6bcbcfb0d9d76a055
Luis Semprun <semprunl@gmail.com>
2015-10-06 11:36:55 +0200

    Implement allocation weight model

--- asset-classes-service/app/models/allocation_weight.rb
+++ asset-classes-service/app/models/allocation_weight.rb
@@ -1,2 +1,6 @@
 class AllocationWeight < ActiveRecord::Base
+  belongs_to :asset_class
+  validates :asset_class, :weight, :tolerance_level, presence: true
+  validates :tolerance_level, uniqueness: { scope: :asset_class,
+    message: "should be unique per asset class" }
 end
